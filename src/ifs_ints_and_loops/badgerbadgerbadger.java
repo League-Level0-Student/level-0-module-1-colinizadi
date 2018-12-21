3d61102af0a798133281e5515c9f5e288717298a
League Student <league@iMac-8.attlocal.net>
2018-12-20 16:58:04 -0800

    badgers ip
    badgers my ages and robot in space

---
+++ src/ifs_ints_and_loops/badgerbadgerbadger.java
@@ -0,0 +1,35 @@
+package ifs_ints_and_loops;
+
+public class badgerbadgerbadger {
+public static void main(String[] args) {
+	for (int i = 0; i < 10; i++) {
+		System.out.print("badger, ");
+	
+	}
+	System.out.println();
+	for (int i = 0; i < 2; i++) {
+		System.out.print("mushroom, ");
+	}
+	System.out.println();
+	System.out.print("a ");
+	for (int i = 0; i < 10; i++) {
+		System.out.print("badger, ");
+	
+	}
+	System.out.println();
+	for (int i = 0; i < 2; i++) {
+		System.out.print("mushroom, ");
+		System.out.println();
+		
+		System.out.print("a ");
+		for (int j = 0; j < 10; j++) {
+			System.out.print("badger, ");
+		}
+		
+		}
+		
+		
+		
+	}
+
+}

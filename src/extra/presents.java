69becb1f9510ed97b776e5cb34b94a23b29e4d45
colinizadi <league@WTS-IM-6.attlocal.net>
2018-12-27 17:02:52 -0800

    ddddddddd
    ddddddddddddddd

--- src/extra/presents.java
+++ src/extra/presents.java
@@ -1,5 +1,10 @@
 package extra;
 
-public class presents {
+import javax.swing.JOptionPane;
 
+public class presents {
+public static void main(String[] args) {
+	String R3$T4RT =JOptionPane.showInputDialog("Did you get any type of gift card for Christmas like i did?");
+	
+}
 }

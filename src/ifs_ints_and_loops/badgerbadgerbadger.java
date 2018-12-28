418ecd938f1d2b59ff454534da5bbeae6d652162
colinizadi <league@WTS-IM-6.attlocal.net>
2018-12-27 17:00:44 -0800

    christmas
    #M3RRY CHR1stmas

--- src/ifs_ints_and_loops/badgerbadgerbadger.java
+++ src/ifs_ints_and_loops/badgerbadgerbadger.java
@@ -2,34 +2,28 @@ package ifs_ints_and_loops;
 
 public class badgerbadgerbadger {
 public static void main(String[] args) {
-	for (int i = 0; i < 10; i++) {
-		System.out.print("badger, ");
-	
-	}
-	System.out.println();
-	for (int i = 0; i < 2; i++) {
-		System.out.print("mushroom, ");
-	}
-	System.out.println();
-	System.out.print("a ");
-	for (int i = 0; i < 10; i++) {
-		System.out.print("badger, ");
-	
-	}
-	System.out.println();
-	for (int i = 0; i < 2; i++) {
-		System.out.print("mushroom, ");
-		System.out.println();
-		
-		System.out.print("a ");
-		for (int j = 0; j < 10; j++) {
-			System.out.print("badger, ");
-		}
-		
-		}
-		
-		
-		
-	}
+		for (int o = 0; o < 2; o++) {
 
+			for (int i = 0; i < 10; i++) {
+				System.out.print("badger, ");
+
+			}
+			System.out.println();
+			for (int i = 0; i < 2; i++) {
+				System.out.print("mushroom, ");
+			}
+			System.out.println();
+		}
+			System.out.println();
+			for (int j = 0; j < 4 ;j++) {
+				System.out.print("snake, ");
+			
+
+	
+		
+		
+		
+	
+}
+}
 }
